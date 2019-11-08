f76a15ca8f2ff7a733dfa47fc32247c741c5b9ed
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-11-08 10:24:50 -0500

    paquetes necesarios

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -19,5 +19,8 @@ export class AuthPage implements OnInit {
 /*
 Instalacion paquetes facebook
 
+ionic cordova plugin add cordova-plugin-facebook4 --variable APP_ID="695087810980695" --variable APP_NAME="Timeout"
 
+
+npm install @ionic-native/facebook
 */
