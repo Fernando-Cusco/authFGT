0855843243978d49c5fbabb5d1a237e10f7e413e
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-11-08 10:24:20 -0500

    instalacion plugins auth con facebook

---
+++ src/app/pages/auth/auth.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.page.html',
+  styleUrls: ['./auth.page.scss'],
+})
+export class AuthPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+
+/*
+Instalacion paquetes facebook
+
+
+*/
