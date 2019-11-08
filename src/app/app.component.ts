c08c114a0afb73855afacaead1f99944e54301dd
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-11-08 14:38:52 -0500

    Inicio y cierre de sesión con google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
 @Component({
   selector: 'app-root',
@@ -20,10 +21,19 @@ export class AppComponent {
     isLogged: false
   };
 
+  userGoogle = {
+    displayName: null,
+    email: null,
+    givenName: null,
+    imageUrl: null,
+    isLogged: false
+  };
+
   constructor( private platform: Platform,
                private splashScreen: SplashScreen,
                private statusBar: StatusBar,
-               private facebook: Facebook) {
+               private facebook: Facebook,
+               private google: GooglePlus) {
     this.initializeApp();
   }
 
@@ -37,7 +47,9 @@ export class AppComponent {
 
   authFacebook() {
     this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
+      console.log('Auth Facebook ', response);
       this.facebook.api('me?fields=id,name,email,picture.width(720).as(picture_large)', []).then(profile => {
+        console.log('Auth Facebook ', profile);
         this.userData = {
           email: profile['email'],
           picture: profile['picture_large']['data']['url'],
@@ -60,6 +72,32 @@ export class AppComponent {
 
 
 
+  authGoogle() {
+    this.google.login({}).then(res => {
+      this.userGoogle = {
+        displayName: res['displayName'],
+        email: res['email'],
+        givenName: res['givenName'],
+        imageUrl: res['imageUrl'],
+        isLogged: true
+      };
+      console.log(res);
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
+
+  exitGoogle() {
+    this.google.logout().then((res) => {
+      console.log('Logout Google ',res);
+      this.userGoogle.isLogged = false;
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+
 
 
 
@@ -75,4 +113,14 @@ export class AppComponent {
 Instalacion paquetes facebook
 ionic cordova plugin add cordova-plugin-facebook4 --variable APP_ID="695087810980695" --variable APP_NAME="Timeout"
 npm install @ionic-native/facebook
+*/
+
+
+/*
+Instalacion paquetes Google
+ionic cordova plugin add cordova-plugin-googleplus --variable 
+REVERSED_CLIENT_ID=208511631300-83nt5jkblvrj2d45o89po6e85pour549.apps.googleusercontent.com
+
+npm install @ionic-native/google-plus 
+
 */
