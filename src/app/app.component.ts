2a43c980eb286f574b85abebcffd82ccbeacf38d
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-11-08 11:13:22 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,15 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
+  }
+
+  auth(event) {
+    console.log(event);
+    this.router.navigate(['auth']);
   }
 
   initializeApp() {
@@ -25,3 +32,10 @@ export class AppComponent {
     });
   }
 }
+
+
+/*
+Instalacion paquetes facebook
+ionic cordova plugin add cordova-plugin-facebook4 --variable APP_ID="695087810980695" --variable APP_NAME="Timeout"
+npm install @ionic-native/facebook
+*/
